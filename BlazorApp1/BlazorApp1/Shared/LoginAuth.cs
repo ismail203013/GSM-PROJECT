86b85892584e5eff176499e35d381097bbe6084b
ismail203013 <78367195+ismail203013@users.noreply.github.com>
2023-04-15 19:16:44 +0100

    Footer

---
+++ BlazorApp1/BlazorApp1/Shared/LoginAuth.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BlazorApp1.Shared
+{
+    public class LoginAuth : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
